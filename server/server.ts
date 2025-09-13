c5aa5d6870543e3d4026416ad7934e9eeb7d3b28
Lars F. Menzel <email@lars-menzel.de>
2025-09-13 06:47:01 +0200

    Add `/api/station/:ortExtId/arrivals` API

--- server/server.ts
+++ server/server.ts
@@ -33,7 +33,6 @@ app.get("/api/station/autocomplete", async (req, res) => {
     }
 });
 
-
 app.get("/api/station/:ortExtId/departures", async (req, res) => {
     try {
         const ortExtId = req.params.ortExtId
@@ -47,6 +46,23 @@ app.get("/api/station/:ortExtId/departures", async (req, res) => {
             .json({error: "Failed to fetch data from the other backend"});
 
         console.error("REST Departures failed with: ", req.query, req.params)
+    }
+});
+
+app.get(`/api/station/:ortExtId/arrivals`, async (req, res) => {
+    try {
+
+        const ortExtId = req.params.ortExtId
+        const response = await axios.get(
+            `https://www.bahn.de/web/api/reiseloesung/ankuenfte?datum=2025-09-12&zeit=15:15:58&ortExtId=${ortExtId}&ortId=A%3D1%40O%3DDresden+Hbf%40X%3D13732039%40Y%3D51040562%40U%3D80%40L%3D8010085%40B%3D1%40p%3D1755118759%40i%3DU%C3%97008006050%40&mitVias=true&maxVias=8&verkehrsmittel[]=ICE&verkehrsmittel[]=EC_IC&verkehrsmittel[]=IR&verkehrsmittel[]=REGIONAL`,
+        );
+        res.json(response.data);
+    } catch (error) {
+        res
+            .status(500)
+            .json({error: "Failed to fetch data from the other backend"});
+
+        console.error("REST Arrivals failed with: ", req.query, req.params)
     }
 });
 
